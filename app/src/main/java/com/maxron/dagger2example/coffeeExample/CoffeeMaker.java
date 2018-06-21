ae486162fd7f2ceaf0bf1945ba57cee6e7503a5b
Wei <zero.maxima@gmail.com>
2018-06-21 11:30:41 +0800

    [Add] Add Lazy usage: after call method

--- app/src/main/java/com/maxron/dagger2example/coffeeExample/CoffeeMaker.java
+++ app/src/main/java/com/maxron/dagger2example/coffeeExample/CoffeeMaker.java
@@ -35,13 +35,48 @@ public class CoffeeMaker {
     }
 
     public void brew() {
+        Log.d(TAG, "brew: ");
+        heater.on();
+        Log.d(TAG, " heater on");
+        heater.on();
+        Log.d(TAG, " heater on");
+        pump.get().pump();
+        Log.d(TAG, " pump pump");
+        pump.get().pump();
+        Log.d(TAG, " pump pump");
         Log.d(TAG, " [_]P coffee! [_]P ");
+        iceBox.get().addIce();
+        Log.d(TAG, " iceBox addIce");
+        iceBox.get().addIce();
+        Log.d(TAG, " iceBox addIce");
+        milk.addMilk();
+        heater.off();
         /*
             Result:
-                provideHeater:
-                provideString:
-                provideShuiGuoMilk:
-                [_]P coffee! [_]P
+                D/CoffeeModule: provideHeater:  // Provide once
+                D/CoffeeModule: provideString:
+                                provideShuiGuoMilk:
+                D/CoffeeMaker: brew:
+                D/ElectricHeater: ~ ~ ~ heating ~ ~ ~
+                D/CoffeeMaker:  heater on
+                D/ElectricHeater: ~ ~ ~ heating ~ ~ ~
+                D/CoffeeMaker:  heater on
+                D/CoffeeModule: providePump:    // Provide once
+                D/Thermosiphon: => => pumping => =>
+                D/CoffeeMaker:  pump pump
+                D/Thermosiphon: => => pumping => =>
+                D/CoffeeMaker:  pump pump
+                                [_]P coffee! [_]P
+                D/CoffeeModule: provideIce: // First provide
+                D/CoffeeModule: provideIceBox: // First provide
+                D/NajiIce: @@ ~~ Ice ~~ @@
+                D/CoffeeMaker:  iceBox addIce
+                 D/CoffeeModule: provideIce: // Second provide
+                D/CoffeeModule: provideIceBox: // Second provide
+                D/NajiIce: @@ ~~ Ice ~~ @@
+                D/CoffeeMaker:  iceBox addIce
+                D/Milk: ** Add Milk: caomei **
+                D/ElectricHeater: off
          */
     }
 }
