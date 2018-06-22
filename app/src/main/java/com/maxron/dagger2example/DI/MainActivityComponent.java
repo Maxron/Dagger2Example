dc3d091e71a195484dd54f8e245843573f8f161f
Wei <zero.maxima@gmail.com>
2018-06-22 17:05:21 +0800

    [Add] Add MainActivity Component

---
+++ app/src/main/java/com/maxron/dagger2example/DI/MainActivityComponent.java
@@ -0,0 +1,13 @@
+package com.maxron.dagger2example.DI;
+
+import com.maxron.dagger2example.MainActivity;
+
+import dagger.Subcomponent;
+import dagger.android.AndroidInjector;
+
+@Subcomponent(modules = MainActivityModule.class)
+public interface MainActivityComponent extends AndroidInjector<MainActivity>{
+
+    @Subcomponent.Builder
+    abstract class Builder extends AndroidInjector.Builder<MainActivity> {}
+}
---
+++ app/src/main/java/com/maxron/dagger2example/DI/MainActivityModule.java
@@ -0,0 +1,7 @@
+package com.maxron.dagger2example.DI;
+
+import dagger.Module;
+
+@Module
+public class MainActivityModule {
+}
