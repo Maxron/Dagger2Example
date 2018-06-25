d549cd4372dd2ab7e5dbc958b2423b8095e6cf24
Wei <zero.maxima@gmail.com>
2018-06-25 11:23:45 +0800

    [Add] Add fragment

---
+++ app/src/main/java/com/maxron/dagger2example/DummyFragment.java
@@ -0,0 +1,55 @@
+package com.maxron.dagger2example;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DummyFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DummyFragment extends Fragment {
+
+    @BindView(R.id.display)
+    TextView display;
+    Unbinder unbinder;
+
+    public DummyFragment() {
+        // Required empty public constructor
+    }
+
+    public static DummyFragment newInstance() {
+        DummyFragment fragment = new DummyFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dummy, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
